46d9e24e8ee4c232fa396f1046e86602f3959b8a
DashaGolubetz <Daria.Golubetz@yandex.ru>
2023-05-23 22:27:18 +0300

    The initial commit.

---
+++ src/main/java/com/github/DashaGolubetz/online_clothes_store/OnlineClothesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.DashaGolubetz.online_clothes_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClothesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClothesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/DashaGolubetz/online_clothes_store/OnlineClothesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.DashaGolubetz.online_clothes_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClothesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
