c4bffa9e295a758a9c6708586a38e083b243eeae
DashaGolubetz <Daria.Golubetz@yandex.ru>
2023-05-28 14:30:59 +0300

    Добавление валидации подтверждения пароля.

--- src/main/java/com/github/DashaGolubetz/online_clothes_store/controllers/AuthController.java
+++ src/main/java/com/github/DashaGolubetz/online_clothes_store/controllers/AuthController.java
@@ -2,10 +2,12 @@ package com.github.DashaGolubetz.online_clothes_store.controllers;
 
 import com.github.DashaGolubetz.online_clothes_store.dtos.UserDTO;
 import com.github.DashaGolubetz.online_clothes_store.services.UserService;
+import com.github.DashaGolubetz.online_clothes_store.validators.UserDTOValidator;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "")
 public class AuthController {
     private final UserService userService;
+    private final UserDTOValidator userDTOValidator;
 
     @Contract(pure = true)
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, UserDTOValidator userDTOValidator) {
         this.userService = userService;
+        this.userDTOValidator = userDTOValidator;
     }
 
     @GetMapping(value = "/register")
@@ -29,7 +33,11 @@ public class AuthController {
     }
 
     @PostMapping(value = "/register")
-    public String registerProcess(@ModelAttribute(value = "user") UserDTO userDTO) {
+    public String registerProcess(@ModelAttribute(value = "user") UserDTO userDTO, BindingResult bindingResult) {
+        userDTOValidator.validate(userDTO, bindingResult);
+
+        if (bindingResult.hasErrors()) return "auth/register";
+
         userService.save(userDTO);
 
         return "redirect:/login";
